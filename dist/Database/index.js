a7393cdf71b577ecacb963ea698f9e00b2347ec1
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2019-08-29 18:53:56 +0300

    fixed last db element search

--- dist/Database/index.js
+++ dist/Database/index.js
@@ -21,6 +21,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var databaseCollectionStores = new WeakMap();
@@ -121,7 +123,9 @@ var Database = exports.Database = function () {
       var _getCollection4 = this.getCollection(collectionName),
           records = _getCollection4.records;
 
-      return (0, _lodash.last)(records);
+      return (0, _lodash.last)([].concat(_toConsumableArray(records)).sort(function (l, r) {
+        return l.id - r.id;
+      }));
     }
   }, {
     key: "push",
